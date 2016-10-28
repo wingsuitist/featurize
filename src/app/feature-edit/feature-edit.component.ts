26692b1bb21b11cfda878b01cde7d8903e3b31a5
Jonas Felix <jonas.felix@felixideas.ch>
2016-10-28 18:46:17 +0200

    basic injections feature top

--- src/app/feature-edit/feature-edit.component.ts
+++ src/app/feature-edit/feature-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { Feature, FeatureService } from '../shared';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'ftr-feature-edit',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feature-edit.component.css']
 })
 export class FeatureEditComponent implements OnInit {
+  feature: Feature;
+  id: number;
 
-  constructor() { }
+  constructor(private featureService: FeatureService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (parameters) => {
+        this.featureService.getFeature(parameters["id"])
+          .subscribe(feature => this.feature = feature);
+      });
   }
 
 }
