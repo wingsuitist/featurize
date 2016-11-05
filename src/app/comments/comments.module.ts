9058d1f76626689aa07e11aa48e06a10e439e77e
Jonas Felix <jonas.felix@felixideas.ch>
2016-11-05 12:30:32 +0000

    basic routing module

---
+++ src/app/comments/comments.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommentsComponent } from './comments.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CommentsComponent]
+})
+export class CommentsModule { }
