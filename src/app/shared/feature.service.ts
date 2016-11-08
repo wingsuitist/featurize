0262f84b8a4b9f7264a5694ef94f1c168d0111f7
Jonas Felix <jonas.felix@felixideas.ch>
2016-11-08 12:19:07 +0100

    add throw to observable

--- src/app/shared/feature.service.ts
+++ src/app/shared/feature.service.ts
@@ -5,6 +5,7 @@ import { Feature } from '../shared'
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class FeatureService {
